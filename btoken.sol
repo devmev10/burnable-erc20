32ed23dca30f935a6595669c09fa09a929f715e0
Ravi Tokas <ravitokas90@gmail.com>
2023-12-26 11:41:11 +0530

    update btoken.sol

--- btoken.sol
+++ btoken.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract Btoken {}
+contract Btoken {
+    /* 
+variables - 
+1. name
+2. symbol
+3. decimals
+*/
+    string public name;
+    string public symbol;
+    uint public decimals;
+
+    //functions
+}
