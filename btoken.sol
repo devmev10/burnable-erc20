cb731d97ee87d339b0f380c98e3fd88a7a8b5002
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2024-01-03 21:01:05 -0800

    add allowance mapping

--- btoken.sol
+++ btoken.sol
@@ -2,22 +2,26 @@
 pragma solidity ^0.8.20;
 
 contract Btoken {
-    /* 
-variables - 
-1. name
-2. symbol
-3. decimals
-*/
+        /* 
+    variables - 
+    1. name
+    2. symbol
+    3. decimals
+    4. totalSupply
+    */
     string public name = "BTOKEN";
     string public symbol = "BTK";
     uint public decimals = 18;
     uint public totalSupply;
 
+    //mappings
+    mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowance;
+
     //The constructor
     constructor() {}
 
-    //mappings
-    mapping(address => uint) public balances;
+    
 
     //functions (read)
     function balanceOf() public view returns (uint) {}
